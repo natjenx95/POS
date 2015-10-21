d7c8279bf1107c60d8fde794ee7e13864a508fe3
arellanoemilio <arellanoemilio@gmail.com>
2015-10-21 00:49:39 -0400

    I added all of the original files to the 'default package'

--- POS/src/Return.java
+++ POS/src/Return.java
@@ -1,3 +1,6 @@
+
+import java.util.ArrayList;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,5 +12,45 @@
  * @author Emilio Arellano
  */
 public class Return {
+    private ArrayList<Item> items;
+    private CustomerClass customer;
     
+    public Return(CustomerClass customer){
+        this.customer = customer;
+        this.items = new ArrayList();
+    }
+    
+    /**
+     * Verifies if the item was sold more than a month ago or
+     * if rented if its returned before the due date or not
+     * @param item the item to be checked
+     * @return the amount of days past due date or negative numbers if due
+     * date hasn't passed
+     */
+    public int VerifyDueDate(Item item){
+       return 0; 
+    }
+    
+    /**
+     * Marks item as returned and updates inventory
+     * @param item 
+     */
+    public void markAsReturned(Item item){
+        
+    }
+    
+    /**
+     * approves the return order
+     */
+    public void approve(){
+        
+    }
+    
+    /**
+     * 
+     * @return the amount due based on the days passed after the return date
+     */
+    public int calculateReturnPayment(){
+        return 0;
+    }
 }
